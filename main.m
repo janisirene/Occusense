a4590c35b5882e36bf25ba6ccf7a6a5e983b82ab
Janis <jintoy@bu.edu>
2017-04-09 21:08:36 -0400

    added figure to report

--- main.m
+++ main.m
@@ -33,7 +33,7 @@ subplot(2, 1, 2); hold on;
 yl = [22, 27];
 mmv = squeeze(mean(mean(v, 1), 2));
 pcolor(1:size(v, 3), yl(1):yl(2), repmat(0+isBackground', [diff(yl)+1, 1]));
-shading flat; caxis([-2 1]);
+shading flat; caxis([-2 1]); colormap gray;
 hMn = plot(mmv, 'b');
 hVert = plot([0, 0], yl, 'r-', 'linewidth', 2);
 for ei = 1:size(events, 1)
@@ -53,6 +53,31 @@ for ti = 1:size(v, 3)
     pause(0.1);
 end
 if doWrite, close(vw); end
+
+%% plot results of person detection
+figure(100); clf; set(gcf, 'Color', 'w');
+subplot(2, 1, 1); hold on;
+pcolor(1:size(v, 3), yl(1):yl(2), repmat(0+isBackground', [diff(yl)+1, 1]));
+shading flat; caxis([-2 1]); colormap gray;
+plot(1:length(mmv), mmv, 'b', 'linewidth', 2);
+xlim([1, length(mmv)]);
+xlabel('time index'); ylabel('temperature');
+title('mean frame temperature');
+for ei = 1:size(events, 1)
+    text(events(ei, 1)+1, yl(1) + .85 * diff(yl), num2str(events(ei, 2)),...
+        'FontSize', 16, 'FontWeight', 'bold');
+end
+set(gca, 'FontSize', 14, 'FontWeight', 'bold');
+
+subplot(2, 1, 2); hold on;
+yl2 = [0, 40];
+pcolor(1:size(v, 3), yl2(1):yl2(2), repmat(0+isBackground', [diff(yl2)+1, 1]));
+shading flat; caxis([-2 1]); colormap gray;
+plot( 1:length(mmv), squeeze(sum(sum(foreground, 1), 2)), 'b', 'linewidth', 2);
+xlim([1, length(mmv)]);
+xlabel('time index'); ylabel('#');
+title('number of detected foreground pixels');
+set(gca, 'FontSize', 14, 'FontWeight', 'bold');
 
 %% play the video (with background subtraction output)
 figure(2); clf; set(gcf, 'Color', 'w');
