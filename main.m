5d05cefc64ae8d450cfb9d5ea65c5977202a1aca
Janis <jintoy@bu.edu>
2017-04-19 08:07:10 -0400

    update main to use playVideo function

--- main.m
+++ main.m
@@ -8,8 +8,8 @@ doWrite = false; % write videos or not
 minFrames = 500;
 pr = 0.3; % probability of motion (smaller is easier)
 
-[v, events, peopleCount, isBackground] = simulateLongVideo(minFrames, pr);
-% load('exampleSimulation.mat');
+% [v, events, peopleCount, isBackground] = simulateLongVideo(minFrames, pr);
+load('exampleSimulation.mat');
 
 %% background subtraction
 backsubParams = struct(...
@@ -28,33 +28,15 @@ foreground = backgroundSubtraction(v, backsubParams);
 mmv = squeeze(mean(mean(v, 1), 2));
 yl = round([min(mmv)-1, max(mmv) + 1]);
 
-%% play the video
-figure(1); clf; set(gcf, 'Color', 'w');
-subplot(2, 1, 1);
-himg = imshow(v(:, :, 1), [20, 30]);
+%% play the original video
+groundTruth = struct('isBackground', isBackground, 'events', events,...
+    'frameMeans', mmv);
+playVideo(v, [], groundTruth);
 
-subplot(2, 1, 2); hold on;
-pcolor(1:size(v, 3), yl(1):yl(2), repmat(0+isBackground', [diff(yl)+1, 1]));
-shading flat; caxis([-2 1]); colormap gray;
-hMn = plot(mmv, 'b');
-hVert = plot([0, 0], yl, 'r-', 'linewidth', 2);
-for ei = 1:size(events, 1)
-    text(events(ei, 1)+1, yl(1) + .75 * diff(yl), num2str(events(ei, 2)),...
-        'FontSize', 14);
-end
-xlim([0, size(v, 3)]);
-xlabel('time index');
-title('mean frame temperature');
-ylim(yl);
-
-if doWrite, vw = VideoWriter('exampleVideo.avi'); open(vw); end
-for ti = 1:size(v, 3)
-    set(himg, 'CData', v(:, :, ti));
-    set(hVert, 'XData', [ti, ti]);
-    if doWrite, writeVideo(vw, getframe(1));end
-    pause(0.1);
-end
-if doWrite, close(vw); end
+%% play the video (with background subtraction output)
+computed = struct('foreground', foreground, 'frameFeature',...
+    squeeze(sum(sum(foreground, 1), 2)));
+playVideo(v, [], groundTruth, computed);
 
 %% plot results of person detection
 figure(100); clf; set(gcf, 'Color', 'w');
@@ -80,41 +62,3 @@ xlim([1, length(mmv)]);
 xlabel('time index'); ylabel('#');
 title('number of detected foreground pixels');
 set(gca, 'FontSize', 14, 'FontWeight', 'bold');
-
-%% play the video (with background subtraction output)
-figure(2); clf; set(gcf, 'Color', 'w');
-subplot(2, 1, 1); hold on;
-himg = imshow(v(:, :, 1), [20, 30]);
-hDots = plot(0, 0, 'r.', 'markersize', 20);
-
-subplot(2, 1, 2); hold on;
-ax1 = gca;
-pcolor(1:size(v, 3), yl(1):yl(2), repmat(0+isBackground', [diff(yl)+1, 1]));
-shading flat; caxis([-2 1]); colormap gray;
-[hAx, hMn, hN] = plotyy(1:length(mmv), mmv,...
-    1:length(mmv), squeeze(sum(sum(foreground, 1), 2)));
-hVert = plot(hAx(1), [0, 0], yl, 'r-', 'linewidth', 2);
-for ei = 1:size(events, 1)
-    text(events(ei, 1)+1, yl(1) + .75 * diff(yl), num2str(events(ei, 2)),...
-        'FontSize', 14);
-end
-xlim([0, size(v, 3)]);
-xlabel('time index');
-title('mean frame temperature');
-set([ax1, hAx(1)], 'YLim', yl);
-set([ax1, hAx], 'XLim', [1, length(mmv)]);
-set(hMn, 'Color', 'b');
-set(hAx(1), 'YTick', yl(1):yl(2));
-ylabel(hAx(1), 'mean frame temp');
-ylabel(hAx(2), '# detected foreground pixels');
-
-if doWrite, vw = VideoWriter('exampleVideoBS.avi'); open(vw); end
-for ti = 1:size(v, 3)
-    [r, c] = find(foreground(:, :, ti));
-    set(hDots, 'XData', c, 'YData', r);
-    set(himg, 'CData', v(:, :, ti));
-    set(hVert, 'XData', [ti, ti]);
-    if doWrite, writeVideo(vw, getframe(2));end
-    pause(0.1);
-end
-if doWrite, close(vw); end
