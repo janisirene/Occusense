c058e72bb36efc6fac57e4969f00656d43588981
Janis <jintoy@bu.edu>
2017-04-19 08:06:39 -0400

    add simpler background subtraction method, add function to play video

---
+++ backgroundSubtractionSimple.m
@@ -0,0 +1,69 @@
+function out = backgroundSubtractionSimple(v, params)
+
+%% parameters
+if ~exist('params', 'var')
+    N = 20; % use N recents frames to estimate background PDF
+    thr = 2.5; % std above which is foreground
+    etaOrder = 1; % order of spatial neighborhood - 1 or 2 for now
+    nIterations = 3; % number of iterations of likelihood ratio testing - 2 or 3
+    gamma = 1; % small -> strong influence of MRF
+    rho = .1; % weight of most recent frame
+    
+    % plotting parameters
+    doPlot = true; % flag to plot results or not   
+else
+    N = params.nBackgroundFrames;
+    thr = params.threshold;
+    etaOrder = params.neighborhoodOrder;
+    nIterations = params.nIterations;
+    gamma = params.gamma;
+    rho = params.rho;
+    doPlot = params.doPlot;
+end
+colorLims = [20, 30];
+
+%% initial conditions
+[~, ~, nFrames] = size(v);
+mu = mean(v(:, :, 1:N), 3);
+vr = var(v(:, :, 1:N), [], 3);
+
+switch etaOrder % (relative indices)
+    case 1 % first order neighborhood
+        eta = [0 1 0; 1 0 1; 0 1 0];
+    case 2 % second order neighborhood
+        eta = [1 1 1; 1 0 1; 1 1 1];
+end
+seta = sum(eta(:));
+
+out = zeros(size(v));
+
+%% loop through frames
+for ti = N+1:nFrames
+    thisFrame = v(:, :, ti);
+    
+    mut = rho * thisFrame + (1 - rho ) * mu;
+    vrt = rho  * (thisFrame-mut).^2 + (1-rho ) * vr;
+    
+    gs = (thisFrame - mut) ./ sqrt(vrt);
+    e = gs > thr;
+    
+    for ii = 1:nIterations
+        mut = e .* mu + (1 - e) .* (rho  * thisFrame + (1 - rho ) * mu);
+        vrt = e .* vr + (1 - e) .* (rho * (thisFrame-mut).^2 + (1-rho ) * vr);
+        gs = (thisFrame - mut) ./ sqrt(vrt);
+        
+        neighFore = imfilter(e, eta);
+        deltaQ = 2 * seta - neighFore;
+        
+        adThr = thr * exp(deltaQ / gamma);
+        
+        e = gs > adThr;
+    end
+    
+    % update running gaussian pdf if pixel was background
+    mu = e .* mu + (1 - e) .* (rho  * thisFrame + (1 - rho ) * mu);
+    vr = e .* vr + (1 - e) .* (rho * (thisFrame-mut).^2 + (1-rho ) * vr);
+    
+    out(:, :, ti) = (thisFrame - mu) ./ sqrt(vr);
+end
+end
---
+++ playVideo.m
@@ -0,0 +1,76 @@
+function playVideo(v, params, groundTruth, computed)
+[h, w, nFrames] = size(v);
+
+mmv = squeeze(mean(mean(v, 1), 2));
+yl = round([min(mmv)-1, max(mmv) + 1]);
+
+if ~exist('params', 'var') || ~isempty(params)
+     clim = params.clim;
+     doWrite = params.doWrite;
+else
+    clim = quantile(v(:), [.1, .9]);
+    doWrite = false;
+end
+
+% ground truth parameters if you have them
+if exist('groundTruth', 'var')
+    isBackground = groundTruth.isBackground;
+    events = groundTruth.events;
+    mmv = groundTruth.frameMeans;
+else
+    isBackground = true(nFrames, 1);
+    events = [];
+    mmv = [];
+end
+
+% foreground if you have it
+if exist('computed', 'var')
+    foreground = computed.foreground;
+    foreFeature = computed.frameFeature;
+else
+    foreground = [];
+    foreFeature = squeeze(mean(mean(v, 1), 2));
+end
+
+
+figure(123); clf; set(gcf, 'Color', 'w');
+subplot(2, 1, 1); hold on;
+himg = imshow(v(:, :, 1), clim);
+hDots = plot(-1, -1, 'r.', 'markersize', 20);
+xlim([1, w]); ylim([1, h]);
+
+subplot(2, 1, 2); hold on;
+ax1 = gca;
+pcolor(1:size(v, 3), yl(1):yl(2), repmat(0+isBackground', [diff(yl)+1, 1]));
+shading flat; caxis([-2 1]); colormap gray;
+[hAx, hMn, hN] = plotyy(1:length(mmv), mmv,...
+    1:length(mmv), foreFeature);
+hVert = plot(hAx(1), [0, 0], yl, 'r-', 'linewidth', 2);
+for ei = 1:size(events, 1)
+    text(events(ei, 1)+1, yl(1) + .75 * diff(yl), num2str(events(ei, 2)),...
+        'FontSize', 14);
+end
+xlim([0, size(v, 3)]);
+xlabel('time index');
+title('mean frame temperature');
+set([ax1, hAx(1)], 'YLim', yl);
+set([ax1, hAx], 'XLim', [1, length(mmv)]);
+set(hMn, 'Color', 'b');
+set(hAx(1), 'YTick', yl(1):yl(2));
+ylabel(hAx(1), 'mean frame temp');
+ylabel(hAx(2), '# detected foreground pixels');
+
+if doWrite, vw = VideoWriter('exampleVideoBS.avi'); open(vw); end
+
+for ti = 1:size(v, 3)
+    if ~isempty(foreground)
+        [r, c] = find(foreground(:, :, ti));
+        set(hDots, 'XData', c, 'YData', r);
+    end
+    set(himg, 'CData', v(:, :, ti));
+    set(hVert, 'XData', [ti, ti]);
+    if doWrite, writeVideo(vw, getframe(2));end
+    pause(0.1);
+end
+if doWrite, close(vw); end
+end
