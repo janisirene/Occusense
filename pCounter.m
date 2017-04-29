13daf852c25a6be42fb99279d041243b52755d94
Janis <jintoy@bu.edu>
2017-04-29 19:22:00 -0400

    max version that gets 83.5% performance

--- pCounter.m
+++ pCounter.m
@@ -1,7 +1,12 @@
 function [pc,startstopdir] = pCounter(v,foreground, thr)
 
 pc = 0;
-iy = mean(v,1);
+%iy = mean(v,1);
+iy = zeros(size(v, 2), 1);
+for i = 1:length(iy)
+    [m, mi] = max(abs(v(:, i)));
+    iy(i) = sign(v(mi, i)) * m; 
+end
 
 event = sum(sum(foreground,1));
 df = diff(event > thr);
@@ -22,6 +27,7 @@ e_len = length(startIdx);
 dirs = zeros(e_len,1);
 for i = 1:e_len
     if iy(startIdx(i)) > iy(stopIdx(i))
+        %     if mean(iy(startIdx(i):startIdx(i)+1)) > iy(stopIdx(i))
         pc = pc -1;
         dirs(i) = -1;
     elseif iy(startIdx(i)) < iy(stopIdx(i))
