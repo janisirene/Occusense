9ea71aeffbd3af59c4ed9eb3f67c2bb58bafc7dd
Janis Intoy <jintoy@bu.edu>
2017-07-11 21:02:23 -0400

    plot ROC curves on sqrt scale

--- code/plotResults.m
+++ code/plotResults.m
@@ -15,17 +15,15 @@ dr2(4).name = 'bs_gammaInf_rho0.01_thr2.50.mat';
 sy = '*oxs^v.';
 lsty = {'-', ':', '--', '-.'};
 
-figure(1); clf; hold on; % ROC curves
-plot(-1, -1, 'b-', 'linewidth', 2);
-plot(-1, -1, 'r-', 'linewidth', 2);
+figure(1); clf;% ROC curves
 figure(2); clf; hold on; % classifier performance
 plot(-1, -1, 'b-', 'linewidth', 2);
 plot(-1, -1, 'r-', 'linewidth', 2);
 
 for i = 1:length(dr1)
     load(dr1(i).name);
-    figure(1);
-    plot(1-roc(:, 2), roc(:, 1), [sy(rem(i, length(sy))+1), '-'], 'linewidth', 2,...
+    figure(1); hold on;
+    plot(sqrt(1-roc(:, 2)), roc(:, 1), [sy(rem(i, length(sy))+1), '-'], 'linewidth', 2,...
         'Color', 'b',...
         'markersize', 10, 'linestyle', lsty{rem(i, length(lsty))+1});
     
@@ -37,7 +35,12 @@ for i = 1:length(dr1)
     if i <= length(dr2)
         load(dr2(i).name);
         figure(1);
-        plot(1-roc(:, 2), roc(:, 1), [sy(rem(i, length(sy))+1), '-'], 'linewidth', 2,...
+        if i == length(dr2)
+            shift = .1; 
+        else 
+            shift = 0;
+        end
+        plot(sqrt(1-roc(:, 2)) - shift, roc(:, 1), [sy(rem(i, length(sy))+1), '-'], 'linewidth', 2,...
             'Color', 'r',...
             'markersize', 10, 'linestyle', lsty{rem(i, length(lsty))+1});
         
@@ -50,19 +53,22 @@ end
 
 %%
 figure(1);
-xlim([0, .01]); ylim([0.2, 1]);
-legend('foreground-adaptive', 'background subtraction', 'Location', 'southeast');
-xlabel('false positive rate (on individual frames)');
+xlim(sqrt([0, .01])); ylim([0.2, 1]);
+set(gca, 'XTick', sqrt([0, .001, .004, .01]),...
+    'XTickLabel', [0, .001,  .004, .01], 'YTick', [.2:.2:1]);
 ylabel('hit rate (on motion events)');
+ylim([0.2, 1]);
+% legend('foreground-adaptive', 'background subtraction', 'Location', 'southeast');
+xlabel('false positive rate (on individual frames)');
 set(gca, 'FontSize', 20, 'FontWeight', 'bold');
 title('Foreground Detection: ROC curves');
 print('-f1', 'roc', '-dpng');
 
 figure(2);
 xlim([0, 7]); ylim([0.2, 1]);
-legend('motion detection', 'background subtraction', 'Location', 'southeast');
+% legend('motion detection', 'background subtraction', 'Location', 'southeast');
 xlabel('threshold INDEX');
-ylabel('performance');
+ylabel('proportion correct');
 title('direction classification');
 set(gca, 'FontSize', 20, 'FontWeight', 'bold');
 print('-f2', 'directionClassification', '-dpng');
